3b756bf7ef77ce7b1ac1834ec9d64ee2b9d93efa
EsraaMahmoudAbdelaziz <esraamahmoud1115@gmail.com>
2025-07-12 18:38:34 +0300

    Q1

--- Exam01Basics/Program.cs
+++ Exam01Basics/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿static void Main(string[] args)
+{
+    
+}
+
+#region Q1
+
+/*Question 1:
+            Write a program that asks the user to enter their age and nationality. If the age is 18 or above
+            and nationality is "Egyptian", print "Eligible to vote", otherwise print "Not eligible"
+**/
+
+
+Console.WriteLine("Enter Your Age");
+int.TryParse(Console.ReadLine(), out int age);
+
+Console.WriteLine("Enter Your Nationality");
+string nationality = Console.ReadLine();
+
+
+if (age == 18 || age > 18 && nationality.ToUpper() == "Egyptian")
+    Console.WriteLine("Eligible to vote");
+else Console.WriteLine("Not eligible");
+#endregion
